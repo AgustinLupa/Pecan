1ab2110384f6ad3a2e43fefe379a4a2a10c9eb70
Matias <matiezelupo@gmail.com>
2023-05-12 16:20:05 -0300

    Search Product Window -Added window to search products -Added the necessary view model -Must update views that work with products -We need a Purchase/Sell Manager

---
+++ Pecan/ViewModels/SearchProductViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Pecan.Entities;
+using Pecan.Utils;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace Pecan.ViewModels;
+
+public class SearchProductViewModel : ViewModelBase
+{
+    private ObservableCollection<CommodityModel> _commodities;
+    private ObservableCollection<CommodityModel> _productList;
+
+    public ICommand FilterCommand { get; set; }
+    public ICommand AddProductCommand { get; set; } 
+
+    public SearchProductViewModel(ObservableCollection<CommodityModel> commodities, ObservableCollection<CommodityModel> productList)
+    {
+        _commodities = commodities;
+        _productList = productList;
+
+        FilterCommand = new RelayCommand(Filter);
+        AddProductCommand = new RelayCommand(AddProduct, CanAddProduct);
+    }
+
+    private bool CanAddProduct(object arg)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void AddProduct(object obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Filter(object obj)
+    {
+        throw new NotImplementedException();
+    }
+}
