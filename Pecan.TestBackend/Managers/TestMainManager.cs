5ac526f86019529ea7e5da61a2cba4c38235865a
Matías Lupo <64920999+matiaslupo@users.noreply.github.com>
2023-08-07 15:13:27 -0300

    Added simple test backend

---
+++ Pecan.TestBackend/Managers/TestMainManager.cs
@@ -0,0 +1,13 @@
+﻿using Pecan.Interface.Contracts;
+
+namespace Pecan.TestBackend.Managers;
+
+public class TestMainManager : IMainManager
+{
+    ICommodityManager _commodityManager= new TestCommodityManager();
+
+    public ICommodityManager GetCommodityManagerInstance()
+    {
+        return _commodityManager;
+    }
+}
